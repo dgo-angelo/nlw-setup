ec7f101efd87729459b3cbfb12c6ad9ce527106d
Diego de Angelo <dgo.angelo@gmail.com>
2023-01-17 20:43:58 -0300

    (feat) [web] adds the base version of the web application.

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import './styles/global.css';
+import { Habit } from './components/Habit';
+
+function App() {
+
+  return (
+    <div>
+      <Habit completed={ 3 } />
+      <Habit completed={ 10 }/>
+      <Habit completed={ 20 }/>
+    </div>
+  );
+}
+
+export default App;
