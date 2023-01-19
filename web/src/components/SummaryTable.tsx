d1eb92ce0241126f6c1c169062516c92ea05e09b
Diego de Angelo <dgo.angelo@gmail.com>
2023-01-18 23:55:53 -0300

    (feat) [web] add summary table, header and habit component.

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,46 @@
+import { HabitDay } from './HabitDay';
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S'
+];
+
+const summaryDates = generateDatesFromYearBeginning();
+const mininumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = mininumSummaryDatesSize - summaryDates.length;
+
+console.log(summaryDates);
+
+export function SummaryTable(){
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          const key = `${weekDay}-${i}`;
+          return (
+            <div key={key} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+              
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_,i) => {
+            return <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />;
+        })}
+      </div>
+    </div>
+  );
+}
